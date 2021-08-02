c77d3cbc9c63d75b8434d826a52e2c53b84e1511
Ross White <rosswhite@outlook.com>
2021-08-02 22:03:55 +0100

    Initial userSeed data

---
+++ server/seeders/seed.js
@@ -0,0 +1,11 @@
+const db = require('../connection/connection');
+const { User } = require('../models');
+const userSeeds = require('./userSeeds.json');
+
+db.once('open', async () => {
+  await User.deleteMany({});
+  await User.create(userSeeds);
+
+  console.log('all done!');
+  process.exit(0);
+});
