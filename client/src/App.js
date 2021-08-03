13ba0ae256ed9a663354b9d0b9aa11f66a8ebea8
Oliver Saxon <oliverrsaxon@gmail.com>
2021-08-03 17:27:17 +0100

    To Do list form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import React, { useState } from 'react';
+import Form from './components/Form';
 
 function App() {
+
+  const [toDos, setTodos] = useState([]);
+
+
   return (
-    <div className="grid grid-cols-3 m-4">
-      <div className="p-4 border flex justify-center col-span-3">
-        <h2 className="text-gray-500 text-bold text-xl">Trip name</h2>
-      </div>
+    <div>
+      <Form onSubmit={text => setTodos([{text, complete: false }, ...toDos])}
+      />
+      <div>
+        {toDos.map(({ text }) => 
+        <div key={text}>{text}</div>
+        )}
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
