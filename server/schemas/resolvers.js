c7ba1ccc5714fcb1ae7061c83d538a3b350dbb39
Ross White <rosswhite@outlook.com>
2021-08-04 22:15:23 +0100

    adding signToken to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Trip } = require('../models');
+const { signToken } = require('../utils/auth');
 
 // Create the functions that fulfill the queries defined in `typeDefs.js`
 const resolvers = {
@@ -15,22 +16,27 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async(parent, { name, email, password }) => {
-      return await User.create({ name, email, password });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    login: async(parent, { email, password }) => {
+    login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-      console.log(user);
+
       if (!user) {
-        throw new AuthenticationError('No user found with that email address');
+        throw new AuthenticationError('No user found with this email address');
       }
 
       const correctPw = await user.isCorrectPassword(password);
-      console.log('Correct Password:::', correctPw);
+
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-      return user;
+
+      const token = signToken(user);
+
+      return { token, user };
     },
     addTrip: async (parent, { organiser, destination, startDate }) => {
       return await Trip.create({ organiser, destination, startDate });
