1eee02c8a4d9419f7a582c2fccd8687921f4b44e
Oliver Saxon <oliverrsaxon@gmail.com>
2021-08-04 21:27:06 +0100

    Sign up form...

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -5,3 +5,16 @@ import { useMutation } from '@apollo/client';
 import { ADD_PROFILE } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+
+const Signup = () => {
+    const [formState, setFormState] = useState({
+      name: '',
+      email: '',
+      password: '',
+    });
+    const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+
+    return ();
+  };
+  
+  export default Signup;
