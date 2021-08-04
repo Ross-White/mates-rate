6c5c2b705cf7dc46bcb9b1cf6bcb5fde68b45223
Ross White <rosswhite@outlook.com>
2021-08-04 16:49:10 +0100

    itinerary typedef updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,9 +8,25 @@ const typeDefs = gql`
     password: String
   }
 
+  type Trip {
+    _id: ID
+    destination: String
+    startDate: Float
+    organiser: ID
+    # guests: []
+    itinerary: [Activity]
+  }
+
+  type Activity {
+    _id: ID
+    date: Float
+    activity: String
+  }
+
   # Define which queries the front end is allowed to make and what data is returned
   type Query {
     users: [User]
+    trips: [Trip]
   }
 `;
 
