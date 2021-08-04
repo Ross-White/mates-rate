262ebc2da0b6ace3de61d5742aef0df91218000a
Oliver Saxon <oliverrsaxon@gmail.com>
2021-08-04 21:30:59 +0100

    Updated App.js to use graphql for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 import SignIn from './components/SignIn';
+import Signup from './components/SignUp';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 function App() {
 
