9df088ec14983ed34e0ec0945991e2d1fd12613a
Oliver Saxon <oliverrsaxon@gmail.com>
2021-08-07 11:59:12 +0100

    removed placeholder divs from home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -6,38 +6,8 @@ const Home = () => {
     return (
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
             {Auth.loggedIn() ? (
-                <>
-                <div>
-                    Logged in
-                </div>
-                <div>
-                Logged in
-                </div>
-                <div>
-                    Logged in
-                </div>
-                <div>
-                Logged in
-                </div>
-                <div>
-                    Logged in
-                </div>
-                <div>
-                Logged in
-                </div>
-                <div>
-                    Logged in
-                </div>
-                <div>
-                Logged in
-                </div>
-                <div>
-                    Logged in
-                </div>
-                <div>
-                Logged in
-                </div>
-                </>
+
+                <h1>You are now logged in</h1>
     
             ) : (
                 <h1>You must log in first</h1>
