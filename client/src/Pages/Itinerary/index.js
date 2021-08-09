279c465f9488bebc086e0a53742e5fea18169026
Ross White <rosswhite@outlook.com>
2021-08-09 21:15:53 +0100

    Activities list displaying

--- client/src/Pages/Itinerary/index.js
+++ client/src/Pages/Itinerary/index.js
@@ -5,16 +5,27 @@ import { QUERY_SINGLE_TRIP } from '../../utils/queries';
 
 const Itinerary = () => {
     const { tripId } = useParams();
-    console.log(tripId);
     const { loading, data } = useQuery(QUERY_SINGLE_TRIP, {
         variables: { tripId: tripId },
     });
 
     const trip = data?.trip || [];
+    console.log(trip.itinerary);
+    const activities = trip.itinerary;
 
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>{trip.destination}</h1>
+            {activities &&
+            activities.map((activity) => (
+                <section className="m-4 h-16" >
+                    <div className="rounded-full flex flex-row justify-between content-center p-4 bg-gray-200" key={activity._id}>
+                        <h4 className="">{activity.activity}</h4>
+
+                        <h4 className="">{activity.date}</h4>
+                    </div>
+                </section>
+            ))}
         </div>
         
     );
--- client/src/Pages/SingleTrip/index.js
+++ client/src/Pages/SingleTrip/index.js
@@ -7,13 +7,11 @@ import { QUERY_SINGLE_TRIP } from '../../utils/queries';
 
 const SingleTrip = () => {
     const { tripId } = useParams();
-    console.log(tripId);
     const { loading, data } = useQuery(QUERY_SINGLE_TRIP, {
         variables: { tripId: tripId },
     });
 
     const trip = data?.trip || [];
-    console.log(trip)
 
     if (loading) {
         return <div>Loading...</div>;
