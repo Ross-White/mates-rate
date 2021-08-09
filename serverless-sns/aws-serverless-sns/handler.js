3f2e5ab7c078710e150a3e0d0a301049517468b3
Luke Drogan <luke@super-wide.com>
2021-08-09 18:39:24 +0100

    adding sns functionality

--- serverless-sns/aws-serverless-sns/handler.js
+++ serverless-sns/aws-serverless-sns/handler.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const body = JSON.parse(event.body)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello World',
+        message: 'Hello World, ' + body.name,
         input: event,
       },
       null,
---
+++ serverless-sns/aws-serverless-sns/sns.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const sns = new AWS.SNS();
+
+const createTopic = async (event) => {
+  /**
+   * Creates an SNS topic
+   * @summary Takes a name and creates an SNS topic
+   * @param {String} event.body.topicName - Name of the topic
+   * @return {JSON} Returns a statusCode, a body that contains a message, topicArn or an Error.
+   */
+  try {
+    const body = JSON.parse(event.body);
+    const params = {
+      Name: body.topicName /* required */,
+    };
+
+    const res = await sns.createTopic(params).promise();
+    console.log(res);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+        {
+          message: `Topic created: ${topicName}`,
+          topicArn: res.TopicArn,
+        },
+        null,
+        2
+      ),
+    };
+
+  } catch (err) {
+    return JSON.stringify(
+        {
+          message: `Topic creation error`,
+          err,
+        },
+        null,
+        2
+      )
+  }
+};
+
+
+module.exports = {
+  createTopic,
+};
