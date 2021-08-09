e6f74b5bb18b92bc47e080586e1e58b0f825842b
Luke Drogan <luke@super-wide.com>
2021-08-09 20:51:58 +0100

    added subscribe functionality & tested

--- aws-serverless-sns/sns.js
+++ aws-serverless-sns/sns.js
@@ -5,16 +5,14 @@ const AWS = require("aws-sdk");
 const sns = new AWS.SNS();
 
 const createTopic = async (event) => {
-  /**
-   * Creates an SNS topic
-   * @summary Takes a name and creates an SNS topic
-   * @param {String} event.body.topicName - Name of the topic
-   * @return {JSON} Returns a statusCode, a body that contains a message, topicArn or an Error.
-   */
+
   try {
     const body = JSON.parse(event.body);
     const params = {
-      Name: body.topicName /* required */,
+      Name: body.topicName,
+      Attributes: {
+        'DisplayName': `Mates Rates - ${body.topicName}`
+      }
     };
 
     const res = await sns.createTopic(params).promise();
@@ -46,6 +44,57 @@ const createTopic = async (event) => {
   }
 };
 
+const subscribeTopic = async ({body}) => {
+  
+  try {
+    const protocol = {
+      email: "email",
+      sms: "sms"
+    };
+    const { topicArn, endpoints } = JSON.parse(body);
+  
+    const promises = endpoints.map(async (endpoint) => {
+
+      const subscribeParams = {
+        Protocol: protocol[endpoint.type],
+        TopicArn: topicArn,
+        Endpoint: endpoint.value,
+        ReturnSubscriptionArn: false,
+      };
+    
+      const res = await sns.subscribe(subscribeParams).promise();
+      return res;
+    });
+    
+    const subRes = await Promise.all(promises);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `Succesfully subscribed to topic!`,
+          responses: subRes,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: `Subscription was unsuccesful.`,
+          err,
+        },
+        null,
+        2
+      ),
+    };
+  }
+}
+
 module.exports = {
   createTopic,
+  subscribeTopic
 };
