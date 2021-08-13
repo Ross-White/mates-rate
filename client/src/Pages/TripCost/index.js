4e17bc777e5b358b1b0bb7fb446d7fc43173586e
Oliver Saxon <oliverrsaxon@gmail.com>
2021-08-13 16:56:34 +0100

    Added handlechange to Trip Cost page; created route from Trip page

--- client/src/Pages/TripCost/index.js
+++ client/src/Pages/TripCost/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import { useQuery, useMutation } from "@apollo/client";
 
 const TripCost = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [formState, setFormState] = useState({
+    description: "",
+    amount: "",
+  });
 
-export default TripCost
+  const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const amount = formState.amount;
+        const description = formState.description;
+    }
+
+
+  return (
+    <div>
+      <h1>Add costs for trip</h1>
+      <form>
+        <label>Amount</label>
+        <input placeholder="£" value={formState.amount} name="amount" onChange={handleChange}></input>
+        <label>Dexcription</label>
+        <input value={formState.description} name="description" onChange={handleChange}></input>
+      </form>
+    </div>
+  );
+};
+
+export default TripCost;
